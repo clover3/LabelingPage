c2626361f33d86fc884d461757b53978bfa53e7b
clover3 <lesterny@gmail.com>
2016-08-26 12:00:01 +0900

    reporter name support

--- load.php
+++ load.php
@@ -87,8 +87,28 @@ var thread_id = <?php echo $thread_id;?>;
 var content = <?php echo $content;?>;
 var keyword = <?php echo $keyword;?>;
 
+function getCookie(cname) {
+	var name = cname + "=";
+	var ca = document.cookie.split(';');
+	for(var i = 0; i <ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length,c.length);
+		}
+	}
+	return "";
+}
+
+function getname() {
+	var username=getCookie("name");
+	return username;
+}
+
 function report(label){
-	reporter = "clover";
+		reporter = getname();
 	url = "report.php?thread_id=" + thread_id + "&article_id=" + article_id
 	+ "&reporter=" + reporter + "&label=" + label + "&corpus_id=" + corpus_id;
 	window.location.href = url;
